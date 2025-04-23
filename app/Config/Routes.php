301494ef17f4bf769b9919926501cdc10e01c42c
Fadhil Musyaffa <musyaffa262@gmail.com>
2025-04-23 12:33:57 +0700

    feat : ....

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -15,11 +15,7 @@ $routes->get('dashboard_user', "Page::dashboard_user");
 $routes->get('ebook', 'Page::ebook');
 $routes->get('profile', 'Page::profile');
 $routes->get('beranda', 'Page::beranda');
-<<<<<<< HEAD
-$routes->get('anggota','Page::data_anggota');
-=======
 $routes->get('anggota', 'Page::data_anggota');
->>>>>>> a20072ad46a2a69587f0fd41306bebfd478e87ef
 $routes->get('buku', 'Page::data_buku');
 $routes->get('buku/tambah_buku', 'Page::tambah_buku');
 $routes->get('buku/edit_buku', 'Page::edit_buku');
@@ -36,49 +32,19 @@ $routes->get('/api/hello/(:any)', 'ApiController::hello/$1');
 
 
 $routes->group('api', ['namespace' => 'App\Controllers'], function ($routes) {
-<<<<<<< HEAD
-    
-    
-=======
 
 
->>>>>>> a20072ad46a2a69587f0fd41306bebfd478e87ef
     // Auth routes
     $routes->post('login', 'AuthController::login');
 
     $routes->get('admins', 'AdminController::index');
-<<<<<<< HEAD
-    $routes->post('admins', 'AdminController::create'); 
-=======
     $routes->post('admins', 'AdminController::create');
->>>>>>> a20072ad46a2a69587f0fd41306bebfd478e87ef
     $routes->get('admins/(:num)', 'AdminController::show/$1');
     $routes->put('admins/(:num)', 'AdminController::update/$1');
     $routes->delete('admins/(:num)', 'AdminController::delete/$1');
 
     // Protected routes (Requires authentication)
     $routes->group('', ['filter' => 'auth'], function ($routes) {
-<<<<<<< HEAD
-         // 🔹 Book Routes
-    $routes->group('books', function ($routes) {
-        $routes->get('/', 'BookController::index');
-        $routes->post('/', 'BookController::create');
-        $routes->get('(:num)', 'BookController::show/$1');
-        $routes->put('(:num)', 'BookController::update/$1');
-        $routes->delete('(:num)', 'BookController::delete/$1');
-        $routes->post('(:num)/upload-cover', 'BookController::uploadCover/$1');
-        $routes->post('(:num)/upload-pdf', 'BookController::uploadPDF/$1');
-    });
-
-    $routes->group('members', function ($routes) {
-        $routes->get('/', 'MemberController::index');
-        $routes->post('/', 'MemberController::create');
-        $routes->get('(:num)', 'MemberController::show/$1');
-        $routes->put('(:num)', 'MemberController::update/$1');
-        $routes->delete('(:num)', 'MemberController::delete/$1');
-        $routes->post('(:num)/upload-profile-picture', 'MemberController::uploadProfilePicture/$1');
-    });
-=======
         // 🔹 Book Routes
         $routes->group('books', function ($routes) {
             $routes->get('/', 'BookController::index');
@@ -99,6 +65,5 @@ $routes->group('api', ['namespace' => 'App\Controllers'], function ($routes) {
             $routes->delete('(:num)', 'MemberController::delete/$1');
             $routes->post('(:num)/upload-profile-picture', 'MemberController::uploadProfilePicture/$1');
         });
->>>>>>> a20072ad46a2a69587f0fd41306bebfd478e87ef
     });
 });
